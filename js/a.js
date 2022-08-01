0ea5f88a8bd0f2f685830af1d0fbcf3291591f8b
AntiRain <dailybreak3@gmail.com>
2022-08-01 16:46:19 +0900

    다른 파일에서 함수 불러오기

---
+++ js/a.js
@@ -0,0 +1,5 @@
+module.exports = {
+    AR15 : () =>{
+        console.log('time to mix drinks and change lives');
+    }
+}
--- js/index.js
+++ js/index.js
@@ -1,5 +1,2 @@
-function func(antirain) {
-    console.log(antirain);
-}
-
-func('time to mix drinks and change lives');
+const a = require("./a.js");
+a.AR15();
