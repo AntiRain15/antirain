308ca6d750a58b093cfa0ee9780b8953717d6da1
AntiRain <dailybreak3@gmail.com>
2022-08-02 15:35:01 +0900

    Create mission5.js

---
+++ mission5/mission5.js
@@ -0,0 +1,8 @@
+const boolean = true;
+const Null = null;
+const Undefined = undefined;
+const num = 457700;
+const bigint = 18n;
+const str = "time to mix drinks and change lives";
+const sym = symbol("이건 대충 심볼");
+const 배열 = ['배열1', '배열2', '배열3']
