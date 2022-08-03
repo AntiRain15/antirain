9946185d4def0dc88499530993c13eca8beebc3f
AntiRain <dailybreak3@gmail.com>
2022-08-03 16:28:49 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res)=>{
-    res.send('실행 중');
+    res.send('get 실행 중');
+});
+
+app.post('/', (req, res)=>{
+    res.send('post 실행 중');
+});
+
+app.patch('/', (req, res)=>{
+    res.send('patch 실행 중');
+});
+
+app.delete('/', (req, res)=>{
+    res.send('send 실행 중');
 });
 
 app.listen(port, () => {});
