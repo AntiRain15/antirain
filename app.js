456993e01c6fecf686a2ce55fe87715021b3d159
AntiRain <dailybreak3@gmail.com>
2022-08-05 16:21:38 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const mysql = require('mysql2/promise')
+
+const connectPool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'swdc',
+    password: '0000',
+});
+
+app.get('/', async (req, res) => {
+    const conn = await connectPool.getConnection();
+    const result = await conn.query('SELECT * FROM `user` WHERE age=14');
+    conn.release()
+    res.send(result[0])
+})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extends: true }))
